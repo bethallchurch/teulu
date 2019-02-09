4bf5688dd29feecedf1cb42cc5ba48d4e75a0924
Beth <bethan.allchurch@gmail.com>
2019-02-09 21:24:00 +0000

    no more todos

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,33 +1,6 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = `mutation CreateTodo($input: CreateTodoInput!) {
-  createTodo(input: $input) {
-    id
-    name
-    description
-    completed
-  }
-}
-`;
-export const updateTodo = `mutation UpdateTodo($input: UpdateTodoInput!) {
-  updateTodo(input: $input) {
-    id
-    name
-    description
-    completed
-  }
-}
-`;
-export const deleteTodo = `mutation DeleteTodo($input: DeleteTodoInput!) {
-  deleteTodo(input: $input) {
-    id
-    name
-    description
-    completed
-  }
-}
-`;
 export const createUser = `mutation CreateUser($input: CreateUserInput!) {
   createUser(input: $input) {
     cognitoId
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,31 +1,6 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = `query GetTodo($id: ID!) {
-  getTodo(id: $id) {
-    id
-    name
-    description
-    completed
-  }
-}
-`;
-export const listTodos = `query ListTodos(
-  $filter: ModelTodoFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      name
-      description
-      completed
-    }
-    nextToken
-  }
-}
-`;
 export const getUser = `query GetUser($id: ID!) {
   getUser(id: $id) {
     cognitoId
