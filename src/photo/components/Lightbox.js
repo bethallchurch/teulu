829496f8cab87d225871b332dbadf0ed90c9d3ed
Beth <bethan.allchurch@gmail.com>
2019-03-28 21:45:22 +0000

    made a start on image slider

--- src/photo/components/Lightbox.js
+++ src/photo/components/Lightbox.js
@@ -1,18 +1,33 @@
 import React, { Component, Children, cloneElement } from 'react'
 import {
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions
 } from 'react-native'
 import { Overlay } from '@global/components'
+import Slider from '@photo/components/ImageSlider'
 import { colors } from '@global/styles'
+
+const { width: WINDOW_WIDTH } = Dimensions.get('window')
 
 class Lightbox extends Component {
   state = { isOpen: false }
 
   getContent = () => {
-    const { children, activeProps } = this.props
-    if (activeProps) {
+    const { children, activeProps, galleryData, galleryStartIndex } = this.props
+    if (activeProps && galleryData.length <= 1) {
       return cloneElement(Children.only(children), activeProps)
+    }
+
+    // TODO: clone child and pass as component to be updated
+    if (activeProps && galleryData.length > 1) {
+      return (
+        <Slider
+          width={WINDOW_WIDTH}
+          position={galleryStartIndex}
+          dataSource={galleryData.map(image => ({ imageProps: image }))}
+        />
+      )
     }
     return children
   }
